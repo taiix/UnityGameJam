8e2f0acdd5f230cb6d4b8df7da9eaf79bde09944
Stan <taix97@outlook.com>
2025-11-09 14:47:12 +0200

    finished room 2

--- Assets/Scripts/CookingPot.cs
+++ Assets/Scripts/CookingPot.cs
@@ -8,6 +8,8 @@ public class CookingPot : MonoBehaviour
     public IngredientType expectedType;
     public int currentIngredientIndex = 0;
 
+    public float cookingTime = 10f; // Time required to cook the dish
+
     private void Start()
     {
         expectedType = ingredientsInPot[0];
@@ -38,6 +40,34 @@ public class CookingPot : MonoBehaviour
                 currentIngredientIndex++;
 
                 Debug.Log($"Added {i.ingredientName} to the pot.");
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent<CookingTripod>(out CookingTripod t))
+        {
+            if (currentIngredientIndex >= ingredientsInPot.Length)
+            {
+                // Start cooking process
+                Debug.Log("Cooking started...");
+                // Here you can implement a timer or coroutine to handle cooking time
+                cookingTime -= Time.deltaTime;
+                if (cookingTime <= 0)
+                {
+                    var child = this.gameObject.transform.GetChild(0).gameObject;
+                    cookingTime = 0;
+                    child.GetComponent<Renderer>().material.color = Color.red;
+                    foreach (Transform grandChild in child.transform)
+                    {
+                        if (grandChild.TryGetComponent<ParticleSystem>(out ParticleSystem p))
+                        { 
+                            var main = p.main;
+                            main.startColor = Color.red;
+                        }
+                    }
+                }
             }
         }
     }
